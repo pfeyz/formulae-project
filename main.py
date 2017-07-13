15ef38f286504c0e0471606d4a9bf46e6cd0fb70
Paul Feitzinger <paul@pfeyz.com>
2017-07-13 15:54:00 -0400

    add subcommand to output top 10 ngrams per file

--- main.py
+++ main.py
@@ -84,30 +84,50 @@ def ngrams(words, n):
     return [words[i:i+n]
             for i in range(len(words) - n + 1)]
 
-def generate_chunks(utterances, gramsize, target_speakers=None):
+def generate_chunks(utterances, gramsize):
     """Returns a generator of ngram strings of size `gramsize`, from list of
     utterance, filtered by speaker.
 
     """
-    for _, speaker, tokens in utterances:
-        if target_speakers is not None and speaker not in target_speakers:
-            continue
+    for uid, speaker, tokens in utterances:
         words = [x.word for x in tokens]
-        include = utterance_filter(words)
-        if include:
-            yield from ngrams(sanitize_words(words), gramsize)
+        if not utterance_filter(words):
+            continue
+        for ngram in ngrams(sanitize_words(words), gramsize):
+            yield uid, speaker, ' '.join(ngram)
 
-def allgrams(filenames, gramsize, speakers=None):
-    """ Prints out all ngrams of size `gramsize` to stdout, filtered by speakers """
+def ngrams_from_files(filenames, gramsize, speakers=None):
     if gramsize < 1:
         raise Exception('Invalid gramsize for allgrams: {}'.format(gramsize))
     parser = MorParser()
     utterances = (utterance
                   for fn in filenames
                   for utterance in parser.parse(fn))
-    chunks = generate_chunks(utterances, gramsize, speakers)
-    for chunk in chunks:
-        print(' '.join(chunk))
+    for _, speaker, gram in generate_chunks(utterances, gramsize):
+        if speakers is None or speaker in speakers:
+            yield gram
+
+def allgrams(filenames, gramsize, speakers=None):
+    """ Prints out all ngrams of size `gramsize` to stdout, filtered by speakers """
+    for ngram in ngrams_from_files(filenames, gramsize, speakers):
+        print(ngram)
+
+def topngrams_from_file(filename, gramsize, freq_cutoff=1, speakers=None):
+    counts = Counter(ngrams_from_files([filename], gramsize, speakers))
+    top_10 = counts.most_common(10)
+    if len(top_10) == 0:
+        raise Exception('No utterances found')
+    cutoff = top_10[-1][1]
+    top_10 = [(item, count)
+              for item, count in counts.items()
+              if count >= cutoff and count > freq_cutoff]
+    return sorted(top_10, key=lambda x: x[1], reverse=True)
+
+def topngrams(filenames, gramsize, speakers=None, freq_cutoff=1):
+    for filename in sorted(filenames):
+        for gram, count in topngrams_from_file(filename, gramsize, speakers=speakers, freq_cutoff=freq_cutoff):
+            print('{}, {}, {}'.format(filename, count, gram))
+        print()
 
 def speakerstats(filenames):
     """ Prints out the number of utterances per speaker in `filenames` """
@@ -136,14 +156,29 @@ def main():
     speakerstats_parser.add_argument('filenames', nargs='+')
     speakerstats_parser.set_defaults(command='speakerstats')
 
+    top10_parser = subparsers.add_parser('top10',
+                                         help='prints the number of utterances per speaker')
+    top10_parser.add_argument('-s', '--speakers', default=None, help='Limit analysis to specific group of speakers, "CHI" or "CHI,MOT"')
+    top10_parser.add_argument('-n', type=int, default=2, help='the size of the ngrams to use')
+    top10_parser.add_argument('-f', '--freq-cutoff', type=int, default=1,
+                              help='Number of times an ngram must occur to appear in output')
+    top10_parser.add_argument('filenames', nargs='+')
+    top10_parser.set_defaults(command='top10')
+
     args = parser.parse_args()
+
+    speakers = None
+    if 'speakers' in args:
+        speakers = args.speakers
+        if speakers is not None:
+            speakers = speakers.split(',')
+
     if 'command' not in args:
         parser.print_help()
     elif args.command == 'speakerstats':
         speakerstats(args.filenames)
+    elif args.command == 'top10':
+        topngrams(args.filenames, args.n, speakers=speakers, freq_cutoff=args.freq_cutoff)
     elif args.command == 'allgrams':
-        speakers = args.speakers
-        if speakers is not None:
-            speakers = speakers.split(',')
         allgrams(args.filenames, args.n, speakers=speakers)
 main()
