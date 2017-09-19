38a29d9fa4898343be1b1efb90e78c2d2d482009
Paul Feitzinger <paul@pfeyz.com>
2017-09-19 17:15:39 -0400

    run morista across all children

--- dataset.py
+++ dataset.py
@@ -38,7 +38,7 @@ def read_data(filenames, gramsize):
 
     return data
 
-def get_dataset(filenames, cached=True):
+def get_dataset(filenames, cached=True, gram_sizes=None):
     """Read xml files specified in `filenames` and returns a dictionary with 3 data
     frames, one for each ngram size of 1, 2 and 3. If data has been loaded
     before, read from a sql DB instead of reparsing XML files.
@@ -54,12 +54,19 @@ def get_dataset(filenames, cached=True):
     DataFrames as values.
 
     """
+
+    gram_sizes = gram_sizes or [1, 2, 3]
+
     conn = sqlite3.connect('cached.sqlite3')
     filenames = sorted(glob(filenames) if isinstance(filenames, str) else filenames)
     table_prefix = path.commonpath(filenames)
     table_prefix = path.basename(table_prefix) or path.dirname(table_prefix)
     data = {}
     ngrams = {'unigram': 1, 'bigram': 2, 'trigram': 3}
+    ngram_rev = {1: 'unigram', 2: 'bigram', 3: 'trigram'}
+    ngrams = {key: val
+              for key, val in ngrams.items()
+              if val in gram_sizes}
     if cached:
         for key in ngrams:
             data[key] = pd.read_sql(
@@ -74,12 +81,14 @@ def get_dataset(filenames, cached=True):
                               conn,
                               if_exists='replace')
     conn.close()
-    return data['unigram'], data['bigram'], data['trigram']
+    if gram_sizes == [1, 2, 3]:
+        return data['unigram'], data['bigram'], data['trigram']
+    return [data[ngram_rev[n]] for n in gram_sizes]
 
-def get_manchester(filenames, cached=True):
+def get_manchester(filenames, cached=True, gram_sizes=None):
     """Calls get_dataset and adds corpus, session and part columns to the resulting
     DataFrames. """
-    frames = get_dataset(filenames, cached)
+    frames = get_dataset(filenames, cached, gram_sizes)
     for data in frames:
         data['corpus'] = data.filename.apply(lambda x: re.sub(r'^.*/|\d+[a-z]\.xml', '', x))
         data['session'] = (data.filename
